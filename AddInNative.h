0f348aa1004b3b3b801968876a761f6b0c76dd91
deshin <deshin_yn@risde.ru>
2023-10-24 23:10:01 +0300

    lgpl preamble added

--- AddInNative.h
+++ AddInNative.h
@@ -1,18 +1,24 @@
-﻿//    This is a part of k-) stack.
-//    Copyright (C) 2023  Yury Deshin, j.deshin@hotmail.com
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as
-//    published by the Free Software Foundation, either version 3 of the
-//    License, or (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+﻿/*
+ * This file is part of k-) stack
+ * Copyright (c) 2023 by Yury Deshin j.deshin@hotmail.com
+ *
+ * This work uses SSH Library (libssh) https://www.libssh.org/
+ * This work based on the external component template source code, written by 1C Company
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #ifndef __ADDINNATIVE_H__
 #define __ADDINNATIVE_H__
